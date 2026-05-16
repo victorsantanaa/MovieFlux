21161e0e29edd02622ac23c4763fd6f9ceb4fe08
Victor Santana <victor.santanaribeiro@gmail.com>
2026-05-16 00:03:09 -0300

    Update MainActivity to use a root NavController and remove unused imports and boilerplate code

--- app/src/main/java/com/example/movieflux/MainActivity.kt
+++ app/src/main/java/com/example/movieflux/MainActivity.kt
@@ -3,14 +3,6 @@ package com.example.movieflux
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.movieflux.navigation.AppNavHost
 import com.example.movieflux.ui.theme.MovieFluxTheme
@@ -19,11 +11,10 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
             MovieFluxTheme {
-                val navController = rememberNavController()
-                AppNavHost(navController)
+                val rootNavController = rememberNavController()
+                AppNavHost(rootNavController)
             }
         }
     }
